5958c6b2a90f273dee6a82c3add63d244a559f6a
Matthew Albrecht <mattparks5855@gmail.com>
2020-02-08 21:51:25 -0600

    Minor tweaks to first implementation.

--- main.cpp
+++ main.cpp
@@ -7,9 +7,10 @@
 int main(int argc, char *argv[]) {
 	using namespace acid;
 
-	Vector2 a(0.5f, -0.5f);
-	Vector2 b(10.0f, 2.0f);
-	Vector3 c(a, 1.0f);
+	Vector2f a(0.5f, -0.5f);
+	a.x() = 0.5f;
+	Vector2f b(10.0f, 2.0f);
+	Vector3f c(a, 1.0f);
 
 	//WRITE_DEBUG("Hello World!");
 	return 0;
